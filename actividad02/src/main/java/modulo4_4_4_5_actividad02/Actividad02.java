ce44ac054bb7c9e199634261537b854448bfe62a
RelatingDarkX <ivacon40@hotmail.com>
2026-07-15 18:59:27 +0200

    Tarea_mod_4.4/4.5

--- actividad02/src/main/java/modulo4_4_4_5_actividad02/Actividad02.java
+++ actividad02/src/main/java/modulo4_4_4_5_actividad02/Actividad02.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Actividad02 {
   public static void main(String[] args) {
     Random rnd = new Random();
-    System.out.println("Random creado");
+    int num = rnd.nextInt(6)+1;
+    System.out.println("Random creado: " + num);
   }
 }
